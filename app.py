51e25de981f2c05d17f529e073759c79def5562f
boronoff <romanticboron@gmail.com>
2023-02-15 23:36:18 +0300

    error with third step

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+api = Api(app)
+movie_ns = api.namespace('movies')
+
 
 class Movie(db.Model):
     __tablename__ = 'movie'
@@ -24,6 +27,7 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +39,41 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        movies = Movie.query.all()
+        return movies_schema.dump(movies)
+
+
+@movie_ns.route('/<int:uid>')
+class MovieView(Resource):
+    def get(self, uid):
+        movie = Movie.query.get(uid)
+        director_id = request.args.get('director_id')
+        if director_id:
+            if Movie.query.join(Director, Director.id == Movie.director_id).filter(Director.id == director_id):
+                movies = Movie.query.join(Director).filter(Director.id == director_id).all()
+                return movies_schema.dump(movies), 200
+            else:
+                return "Error", 404
+        return movie_schema.dump(movie)
 
 
 if __name__ == '__main__':
